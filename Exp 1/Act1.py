1c7d1b6239f595066e01f7e634c0c52f0d47504b
NikhilNikam366 <nikhilnikam291@gmail.com>
2026-02-09 15:24:45 +0530

    Create Act1.py

---
+++ Exp 1/Act1.py
@@ -0,0 +1,17 @@
+num1=int(input("Enter Number1:"))
+num2=int(input("Enter Numbe2r:"))
+
+sum=num1+num2
+print("Addition=",sum)
+
+sum=num1-num2
+print("Substraction=",sub)
+
+sum=num1*num2
+print("Multiplication=",mul)
+
+sum=num1/num2
+print("Division=",div)
+
+sum=num1%num2
+print("Modulus=",mod)
